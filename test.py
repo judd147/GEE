8d22054be081cc001239577148c9c93e5a711ee5
judd147 <57974800+judd147@users.noreply.github.com>
2022-06-14 10:56:10 +0800

    Update test.py

--- test.py
+++ test.py
@@ -16,7 +16,6 @@ vis_params = {
 }
 # Add the Earth Engine image to the map
 Map.addLayer(dem, vis_params, "SRTM DEM", True, 0.5)
-# Add a colorbar to the map
-Map.add_colorbar(vis_params["palette"], 0, 4000, caption="Elevation (m)")
+
 # Render the map using streamlit
 Map.to_streamlit()
